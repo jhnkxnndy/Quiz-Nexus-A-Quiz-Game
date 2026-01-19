d1455ddafeb5a84ca08f1991132a87563f12c55f
jhnkxnndy <j.kxnnedyy@gmail.com>
2026-01-19 14:20:31 +0800

    Edit lang

--- AboutUs.vb
+++ AboutUs.vb
@@ -1,8 +1,9 @@
 ﻿Public Class AboutUs
     Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
-        SettingsFrm.TopLevel = False
+        SettingsFrm.TopLevel = True
         SettingsInterface.Guna2Panel1.Controls.Add(SettingsFrm)
         SettingsFrm.Show()
         SettingsFrm.BringToFront()
+        SettingsFrm.Close()
     End Sub
 End Class
